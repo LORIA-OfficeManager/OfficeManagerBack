08ae8af1c6cbb829c94a126a1f843448f03b2d40
formotiis <nosvulca@gmail.com>
2019-11-13 10:03:15 +0100

    Initial Commit

---
+++ src/main/java/fr/loria/office/App.java
@@ -0,0 +1,13 @@
+package fr.loria.office;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
