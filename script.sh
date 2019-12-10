5a7cfbdb910b48c965d0b1f931c1c2de6557ef3f
Highnewb <thouveninvalentin@gmail.com>
2019-12-10 11:09:38 +0100

    Ajout travis

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /deploy/officemanager-back
+fkill :51000
+docker-compose down
+docker-compose up -d
+sudo chmod -R 777 db_officemanager
+java -Dserver.port=51000 -Dspring.profiles.active=prod -jar target/OfficeManagerBack-1.0-SNAPSHOT
