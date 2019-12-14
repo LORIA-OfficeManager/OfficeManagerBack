39e64a0565eb6ae48b9fd52be65f15584802fbf1
Highnewb <thouveninvalentin@gmail.com>
2019-12-14 14:48:37 +0100

    script deploy

--- script.sh
+++ script.sh
@@ -5,4 +5,4 @@ fkill :51000
 docker-compose down
 docker-compose up -d
 sudo chmod -R 777 db_officeManager
-java -Dserver.port=51000 -Dspring.profiles.active=prod -jar target/OfficeManager-0.0.1-SNAPSHOT.jar
+java -Dserver.port=51000 -jar target/OfficeManager-0.0.1-SNAPSHOT.jar
