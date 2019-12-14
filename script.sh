1da0b32a7f16824750909ca7a5ba52b0a888feb1
Highnewb <thouveninvalentin@gmail.com>
2019-12-14 11:36:01 +0100

    Fix script & travis

--- script.sh
+++ script.sh
@@ -4,5 +4,5 @@ cd /deploy/officemanager-back
 fkill :51000
 docker-compose down
 docker-compose up -d
-sudo chmod -R 777 db_officeManager
+sudo chmod -R 777 db_officeManager/*
 java -Dserver.port=51000 -Dspring.profiles.active=prod -jar target/OfficeManagerBack-1.0-SNAPSHOT
