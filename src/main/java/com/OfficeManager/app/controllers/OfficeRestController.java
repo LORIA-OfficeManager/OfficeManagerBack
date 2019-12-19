18810a793b753d12fe3d24f92499626d88ecac00
formotiis <nosvulca@gmail.com>
2019-12-19 15:55:29 +0100

    Hotfix: Cross origin

--- src/main/java/com/OfficeManager/app/controllers/OfficeRestController.java
+++ src/main/java/com/OfficeManager/app/controllers/OfficeRestController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/office", produces = MediaType.APPLICATION_JSON_VALUE)
 public class OfficeRestController {
