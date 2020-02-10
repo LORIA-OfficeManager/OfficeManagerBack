c876a4578f9d82665682343c0be645d5061e9204
Highnewb <thouveninvalentin@gmail.com>
2020-02-10 15:24:13 +0100

    New CI

--- script.sh
+++ script.sh
@@ -3,7 +3,5 @@
 cd /deploy/officemanager-back
 fkill :51000
 docker-compose down
-docker-compose up -d
-sudo chmod -R 777 db_officeManager
-java -Dserver.port=51000 -jar target/OfficeManager-0.0.1-SNAPSHOT.jar &
+docker-compose up -d --build
 echo "Build success"
