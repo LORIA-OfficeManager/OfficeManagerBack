d562cff141b14c05aef5eda57f32a4ba8fbe0470
Highnewb <thouveninvalentin@gmail.com>
2020-03-03 16:49:00 +0100

    Déplacement localisation mail receveur

--- src/main/java/com/OfficeManager/app/services/impl/EmailServiceImpl.java
+++ src/main/java/com/OfficeManager/app/services/impl/EmailServiceImpl.java
@@ -2,6 +2,7 @@ package com.OfficeManager.app.services.impl;
 
 import com.OfficeManager.app.services.interfaces.IEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -11,11 +12,14 @@ public class EmailServiceImpl implements IEmailService {
 
     @Autowired
     public JavaMailSender emailSender;
+    @Autowired
+    private Environment env;
+
 
     @Override
     public void sendSimpleMessage(String text) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo("officemanager@yopmail.fr");
+        message.setTo(env.getProperty("emailReceipt"));
         message.setSubject("Erreur OfficeManager");
         message.setText(text);
         emailSender.send(message);
